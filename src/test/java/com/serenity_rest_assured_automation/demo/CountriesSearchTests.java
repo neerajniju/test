a60a150b687f1afb3adb5e0433f02915cc230f9a
Chhikara <130423@NTTDATA.COM>
2018-03-22 15:18:18 +0530

    firsttesting

---
+++ src/test/java/com/serenity_rest_assured_automation/demo/CountriesSearchTests.java
@@ -0,0 +1,35 @@
+package com.serenity_rest_assured_automation.demo;
+
+import net.thucydides.core.annotations.Steps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import steps.CountriesSearchSteps;
+
+@RunWith(SerenityRunner.class)
+public class CountriesSearchTests {
+   @Steps
+   CountriesSearchSteps countriesSearchSteps;
+
+   @Test
+   public void verifyThatWeCanFindUnitedStatesOfAmericaCountryUsingTheCodeUS() {
+       countriesSearchSteps.searchCountryByCode("US");
+       countriesSearchSteps.searchIsExecutedSuccesfully();
+       countriesSearchSteps.iShouldFindCountry("United States of America");
+   }
+
+   @Test
+   public void verifyThatWeCanFindIndiaCountryUsingTheCodeIN(){
+       countriesSearchSteps.searchCountryByCode("IN");
+       countriesSearchSteps.searchIsExecutedSuccesfully();
+       countriesSearchSteps.iShouldFindCountry("India");
+   }
+
+   @Test
+   public void verifyThatWeCanFindBrazilCountryUsingTheCodeBR(){
+       countriesSearchSteps.searchCountryByCode("BR");
+       countriesSearchSteps.searchIsExecutedSuccesfully();
+       countriesSearchSteps.iShouldFindCountry("Brazil");
+   }
+}
